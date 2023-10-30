f745e23ba824447fc5e08087bb70afb79bdaaf43
camiloDanielli <daniellicamilo6@gmail.com>
2023-10-29 21:51:53 -0300

    Principio de Inversion de dependencia

--- src/Library/ConsolePrinter.cs
+++ src/Library/ConsolePrinter.cs
@@ -4,9 +4,10 @@ namespace Full_GRASP_And_SOLID
 {
     public class ConsolePrinter : IPrinter
     {
-        public void PrintRecipe(Recipe recipe)
-        {
-            Console.WriteLine(recipe.GetTextToPrint());
+        public void PrintRecipe(ITextProvider textProvider)
+        {   
+            string  textToPrint = textProvider.GetTextToPrint();
+            Console.WriteLine(textToPrint);
         }
     }
 }
---
+++ src/Library/ITextProvider.cs
@@ -0,0 +1,6 @@
+using System;
+
+public interface ITextProvider
+{
+    String GetTextToPrint();
+}
